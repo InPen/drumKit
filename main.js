f98f3467577e205c72199ccabfc99705708ce375
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 13:58:33 -0400

    Target data-key elements and display them on console

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.addEventListener('keydown', function(event){
-
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`)
+  if(!audio) return //if we do not have an audio element for that key, stop the function
+  console.log(audio)
 })
-console.log('hello world')
