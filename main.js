c981eb0641c36ac84b1c34b84602d293cc428d6a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 14:13:36 -0400

    Highlight in the DOM what key you're playing

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.addEventListener('keydown', function(event){
+  const key = document.querySelector(`.key[data-key='${event.keyCode}']`)
   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`)
   if(!audio) return //if we do not have an audio element for that key, stop the function
-  audio.currentTime = 0
+  audio.currentTime = 0 //rewind key to start so you can play over and over again
   audio.play()
-  console.log(audio)
+  key.classList.add('playing')
 })
