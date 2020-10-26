68ea830abf330f2f570eaf0899157c459158e199
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 14:37:02 -0400

    Include final code with time transitions for CSS animation

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', function(event){
-  const key = document.querySelector(`.key[data-key='${event.keyCode}']`)
-  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`)
-  if(!audio) return //if we do not have an audio element for that key, stop the function
-  audio.currentTime = 0 //rewind key to start so you can play over and over again
-  audio.play()
-  key.classList.add('playing')
-})
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; //if we do not have an audio element for that key, stop the function
+
+    key.classList.add('playing');
+    audio.currentTime = 0; //rewind key to start so you can play over and over again
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
