754b2b1012e00fe95806cacdfb8db6c20d33a61c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 13:59:51 -0400

    PLAY your keys!!!

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown', function(event){
   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`)
   if(!audio) return //if we do not have an audio element for that key, stop the function
+  audio.play()
   console.log(audio)
 })
