988d6dbe83922bcfdec947888d9c0137718b5dcb
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-26 14:01:06 -0400

    Rewind key to 0 so it can be played over and over again without having to wait

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function(event){
   const audio = document.querySelector(`audio[data-key='${event.keyCode}']`)
   if(!audio) return //if we do not have an audio element for that key, stop the function
+  audio.currentTime = 0
   audio.play()
   console.log(audio)
 })
